1e4b23a9f5be3e4507279fbfdda36993b5ddb494
kun <938481169@qq.com>
2018-11-18 23:17:15 +0800

    学习日志

--- JavaWeb/src/main/java/log/LoggerDemo.java
+++ JavaWeb/src/main/java/log/LoggerDemo.java
@@ -4,7 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 学习日志框架
+ * 学习日志框架。<br>
+ * 配置文件：log4j.properties
  * 
  * @author 冯仕清
  *
@@ -28,6 +29,8 @@ public class LoggerDemo {
 		System.out.println(LOGGER.isInfoEnabled());
 		
 		LOGGER.info("【么么哒】【开始执行】【亲爱的{}】", "李双");
+		// slf4j 支持占位符，相比log4j 节省内存，性能高效。
+		LOGGER.info("【参数：】【{}，{}，{}，{},{}，{}，{}，{}】", "李双","sdfsadf", "sdf", "fef", "李双","sdfsadf", "sdf", "fef");
 	}
 
 }
