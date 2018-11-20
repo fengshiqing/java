8c3818d912e0d8809444ca623a0c07b13e4d197b
kun <938481169@qq.com>
2018-11-20 23:02:58 +0800

    代码优化

--- JavaSE/src/pattern/Proxy.java
+++ JavaSE/src/pattern/Proxy.java
@@ -3,9 +3,12 @@ package pattern;
 /**
  * 代理模式。<br>
  * 这种是静态代理，就是接口和实现是一一对应的，换个被代理的类，就要新建一个对应的接口。<br>
- * 后面学反射了可以用动态代理。<br>
+ * 后面学了反射就可以用动态代理。<br>
  * 
- * 静态代理思想：一个公共接口，两个实现类，其中一个代理类，另一个是被代理类，代理类中创建被代理类的引用，并调用被代理类的方法。
+ * 静态代理思想：<br>
+ * 1、一个公共接口，<br>
+ * 2、两个实现类，其中一个代理类，另一个是被代理类，<br>
+ * 3、代理类中创建被代理类的引用，并调用被代理类的方法。
  */
 public class Proxy {
 
@@ -16,30 +19,32 @@ public class Proxy {
 
 }
 
-// 接口
+// 一个公共接口
 interface IAction {
 	void action();
 }
 
 // 代理类
 class ProxyObject implements IAction {
-	IAction iActionObj;
+
+	IAction iActionObj;// 代理类中创建被代理类的引用
 
 	public ProxyObject() {
 		System.out.println("代理类创建成功");
-		iActionObj = new ActionImpl();
+		iActionObj = new ActionImpl();// 代理类中创建被代理类的引用
 	}
 
 	@Override
 	public void action() {
 		System.out.println("代理类开始执行");
-		iActionObj.action();
+		iActionObj.action();// 调用被代理类的方法
 		System.out.println("代理类结束执行");
 	}
 }
 
 // 被代理类
 class ActionImpl implements IAction {
+
 	@Override
 	public void action() {
 		System.out.println("被代理类开始执行");
