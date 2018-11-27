dff7cf98cd7d29bbdb4e1b4982e4ea23e94aa89f
kun <938481169@qq.com>
2018-11-27 23:20:28 +0800

    springMVC

--- JavaWeb/src/main/java/springIoC/SpringIOC.java
+++ JavaWeb/src/main/java/springIoC/SpringIOC.java
@@ -5,13 +5,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import beans.HelloWorld;
 
+/**
+ * 
+ * @see
+ */
 public class SpringIOC {
-	
+
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 		obj.getMessage();
+		
+		
+		
+		
 	}
 
 }
