d2b4bb4de6ea09a3026175febde0bf3fb6d6e50a
fengshiqing <938481169@qq.com>
2018-11-29 09:49:25 +0800

    Update HelloController.java

--- JavaWeb/src/main/java/springMVC/HelloController.java
+++ JavaWeb/src/main/java/springMVC/HelloController.java
@@ -1,16 +1,28 @@
 package springMVC;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/Spring/hello")
+@RequestMapping("/Spring")
 public class HelloController {
 
-	@RequestMapping(method = RequestMethod.GET)
+	/**
+	 * <日志>
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+	
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
 	public String printHello(ModelMap model) {
+		LOGGER.info("【printHello】【开始执行】");
+		LOGGER.info("【printHello】【开始执行】【路径：{}】", HelloController.class.getResource(""));
+		LOGGER.info("【printHello】【开始执行】【路径：{}】", HelloController.class.getResource("/"));
+		LOGGER.info("【printHello】【开始执行】【路径：{}】", HelloController.class.getClassLoader().getResource(""));
+		
 		model.addAttribute("message", "Hello Spring MVC Framework!!!");
 		return "hello";
 	}
