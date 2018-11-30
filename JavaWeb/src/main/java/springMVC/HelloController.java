9ff2a5eb14f7d4dfeb46102a743889b0ac463e64
fengshiqing <938481169@qq.com>
2018-11-30 14:05:28 +0800

    Update HelloController.java

--- JavaWeb/src/main/java/springMVC/HelloController.java
+++ JavaWeb/src/main/java/springMVC/HelloController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,5 +27,16 @@ public class HelloController {
 		model.addAttribute("message", "Hello Spring MVC Framework!!!");
 		return "hello";
 	}
-
+	
+	@RequestMapping(value = "/Var/{page}", method = RequestMethod.GET)
+	public String printPage(@PathVariable String page, ModelMap model) {
+	    LOGGER.info("【printHello】【开始执行】");
+	    LOGGER.info("【printHello】【开始执行】【路径：{}】", HelloController.class.getResource(""));
+	    LOGGER.info("【printHello】【开始执行】【路径：{}】", HelloController.class.getResource("/"));
+	    LOGGER.info("【printHello】【开始执行】【路径：{}】", HelloController.class.getClassLoader().getResource(""));
+	    
+	    model.addAttribute("message", page);
+	    return "hello";
+	}
+	
 }
