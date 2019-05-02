dd5c0622dd8be73f5cff13ec69b12b30b4df6d47
fengshiqing <938481169@qq.com>
2019-05-03 01:21:48 +0800

    代码优化

--- Spring-Boot/src/main/java/com/kunning/dao/UserMapper.java
+++ Spring-Boot/src/main/java/com/kunning/dao/UserMapper.java
@@ -2,9 +2,7 @@ package com.kunning.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import com.kunning.pojo.User;
 import org.springframework.stereotype.Repository;
@@ -14,25 +12,25 @@ public interface UserMapper {
 
 	@Select("SELECT * FROM user")
     @Results({
-        @Result(property = "username",  column = "username", javaType = String.class),
+        @Result(property = "username", column = "username", javaType = String.class),
         @Result(property = "nickname", column = "nickname")
     })
     List<User> getAll();
 
-//    @Select("SELECT * FROM users WHERE id = #{id}")
-//    @Results({
-//        @Result(property = "username",  column = "username", javaType = UserSexEnum.class),
-//        @Result(property = "nickname", column = "nickname")
-//    })
-//    UserEntity getOne(Long id);
-//
-//    @Insert("INSERT INTO users(username,password,user_sex) VALUES(#{username}, #{password}, #{userSex})")
-//    void insert(UserEntity user);
-//
-//    @Update("UPDATE users SET userName=#{username},nick_name=#{username} WHERE id =#{id}")
-//    void update(UserEntity user);
-//
-//    @Delete("DELETE FROM users WHERE id =#{id}")
-//    void delete(Long id);
-	
+    @Select("SELECT * FROM user WHERE id = #{id}")
+    @Results({
+        @Result(property = "username", column = "username", javaType = String.class),
+        @Result(property = "nickname", column = "nickname")
+    })
+    User getOne(Long id);
+
+    @Insert("INSERT INTO user(username,password,user_sex) VALUES(#{username}, #{password}, #{userSex})")
+    void insert(User user);
+
+    @Update("UPDATE user SET userName=#{username},nick_name=#{username} WHERE id =#{id}")
+    void update(User user);
+
+    @Delete("DELETE FROM user WHERE id =#{id}")
+    void delete(Long id);
+
 }
