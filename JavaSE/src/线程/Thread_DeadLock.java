b10101ce99675a5ad59fb12487c8ff13764f9c9c
fengshiqing <938481169@qq.com>
2019-05-02 18:47:39 +0800

    代码优化

--- JavaSE/src/线程/Thread_DeadLock.java
+++ JavaSE/src/线程/Thread_DeadLock.java
@@ -4,8 +4,8 @@ package 线程;
  * <死锁>
  */
 public class Thread_DeadLock implements Runnable {
-	private static Zhangsan zs = new Zhangsan(); // 实例化static型对象
-	private static Lisi ls = new Lisi(); // 实例化static型对象
+	private static Zhangsan zs = new Thread_DeadLock().new Zhangsan(); // 实例化static型对象
+	private static Lisi ls = new Thread_DeadLock().new Lisi(); // 实例化static型对象
 	private boolean flag = false; // 声明标志位，判断那个先说话
 
 	@Override
@@ -47,24 +47,24 @@ public class Thread_DeadLock implements Runnable {
 		thA.start();
 		thB.start();
 	}
-}
 
-class Zhangsan { // 定义张三类
-	public void say() {
-		System.out.println("张三对李四说：“你给我画，我就把书给你。”");
+	class Zhangsan { // 定义张三类
+		public void say() {
+			System.out.println("张三对李四说：“你给我画，我就把书给你。”");
+		}
+
+		public void get() {
+			System.out.println("张三得到画了。");
+		}
 	}
 
-	public void get() {
-		System.out.println("张三得到画了。");
-	}
-}
+	class Lisi { // 定义李四类
+		public void say() {
+			System.out.println("李四对张三说：“你给我书，我就把画给你”");
+		}
 
-class Lisi { // 定义李四类
-	public void say() {
-		System.out.println("李四对张三说：“你给我书，我就把画给你”");
-	}
-
-	public void get() {
-		System.out.println("李四得到书了。");
+		public void get() {
+			System.out.println("李四得到书了。");
+		}
 	}
 }
