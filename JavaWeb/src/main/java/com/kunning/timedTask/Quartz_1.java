0f60372db7900def43a899c7981eded68dc6ea61
fengshiqing <938481169@qq.com>
2019-05-09 00:06:20 +0800

    配置定时任务

---
+++ JavaWeb/src/main/java/com/kunning/timedTask/Quartz_1.java
@@ -0,0 +1,13 @@
+package com.kunning.timedTask;
+
+public class Quartz_1 {
+
+    public void task_1() {
+        System.out.println("【task_1】");
+    }
+
+    public void task_2() {
+        System.out.println("【task_2】");
+    }
+
+}
