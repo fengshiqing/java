53ddb6361d672164d9fa8811d6f4dccf1d267444
fengshiqing <938481169@qq.com>
2019-05-09 20:23:52 +0800

    代码优化

---
+++ JavaSE/src/main/java/com/kunning/jdbc/JDBCUtil.java
@@ -0,0 +1,51 @@
+package com.kunning.jdbc;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class JDBCUtil {
+
+	/**
+	 * <存放jdbc的配置信息>
+	 */
+	private static final Properties prop;
+
+	static {// 静态代码块
+		prop = new Properties();
+		try {
+			prop.load(new FileReader("jdbc.properties"));// 这种路径的加载方式是"jdbc.properties"直接放在项目根路径下
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * <获数据库连接>
+	 */
+	public static Connection getConnection() throws Exception {
+		String driverClassName = prop.getProperty("jdbc.driverClassName");// 驱动类全限定名
+		String url = prop.getProperty("jdbc.url");// URL
+		String user = prop.getProperty("jdbc.username");// 用户名
+		String password = prop.getProperty("jdbc.password");// 密码
+		System.out.println("drivdriverClassNameerClass：" + driverClassName);
+		System.out.println("url：" + url);
+		System.out.println("user：" + user);
+		System.out.println("password：" + password);
+		Class.forName(driverClassName);// 注册数据库驱动
+		Connection conn = DriverManager.getConnection(url, user, password);// 获取数据库连接
+		System.out.println(conn);
+		return conn;
+	}
+
+	// 测试
+	public static void main(String[] args) throws Exception {
+		JDBCUtil.getConnection();
+	}
+
+}
---
+++ JavaSE/src/main/java/com/kunning/mybatis/StudentTest.java
@@ -0,0 +1,46 @@
+package com.kunning.mybatis;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.kunning.mybatis.model.Student;
+import com.kunning.mybatis.util.SqlSessionFactoryUtil;
+
+public class StudentTest {
+
+	/**
+	 * <日志>
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(StudentTest.class);
+
+	/**
+	 * <主方法>
+	 */
+	public static void main(String[] args) {
+		LOGGER.info("【开始执行】");
+
+		// 步骤一：获取Session
+		SqlSession sqlSession = SqlSessionFactoryUtil.openSession();// 获取sqlsession
+		
+		// 步骤二：组装数据
+		Student student = new Student("zhangsan", 11);
+
+		// 步骤三：执行sql语句
+		// 执行sql方式一：
+		int result = sqlSession.insert("com.kunning.mybatis.mappers.StudentMapper.add", student);// 这种方式可以不写dao层接口，直接调用。
+
+		// 执行sql方二：
+//		StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);// 这种方式需要dao层接口。sql语句甚至可以写在dao层接口的方法上。
+//		int result = studentMapper.add(student);
+
+		// 步骤四：commit、close
+		sqlSession.commit();// 注释掉这句话，就没有提交，数据库中也就不会有变化
+		sqlSession.close();
+		if (result > 0) {
+			LOGGER.info("【添加成功】");
+		} else {
+			LOGGER.info("【添加失败】");
+		}
+	}
+}
