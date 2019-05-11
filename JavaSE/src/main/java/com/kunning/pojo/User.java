316308cf2c956bc78dffd7111614a7e5cca94f3e
fengshiqing <938481169@qq.com>
2019-05-11 16:04:25 +0800

    代码优化

--- JavaSE/src/main/java/com/kunning/pojo/User.java
+++ JavaSE/src/main/java/com/kunning/pojo/User.java
@@ -1,50 +1,66 @@
 package com.kunning.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@ToString
 public class User {
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 
     /**
      * ID
      */
-    @Setter
-    @Getter
     private int id;
 
     /**
      * 用户名
      */
-    @Setter
-    @Getter
     private String username;
 
     /**
      * 密码
      */
-    @Setter
-    @Getter
     private String password;
 
     /**
      * 昵称
      */
-    @Setter
-    @Getter
     private String nickname;
 
     /**
      * 邮箱
      */
-    @Setter
-    @Getter
     private String email;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+    }
 
 }
