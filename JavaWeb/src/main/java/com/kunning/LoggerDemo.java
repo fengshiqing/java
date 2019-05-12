5f723eaafd9de98f4dc26169260e38114324e87a
fengshiqing <938481169@qq.com>
2019-05-12 09:51:01 +0800

    代码优化

--- JavaWeb/src/main/java/com/kunning/LoggerDemo.java
+++ JavaWeb/src/main/java/com/kunning/LoggerDemo.java
@@ -6,31 +6,27 @@ import org.slf4j.LoggerFactory;
 /**
  * <学习日志框架。><br>
  * <配置文件：log4j.properties>
- * 
- * @author 冯仕清
  *
+ * @author 冯仕清
  */
 public class LoggerDemo {
 
-	/**
-	 * <日志>
-	 */
-	private static final Logger LOGGER = LoggerFactory.getLogger(LoggerDemo.class);
+    /**
+     * <日志>
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerDemo.class);
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String name = Logger.ROOT_LOGGER_NAME;
-		System.out.println(name); // false 说明root无法通过name获取
+    public static void main(String[] args) {
+        String name = Logger.ROOT_LOGGER_NAME;
+        System.out.println(name); // false 说明root无法通过name获取
 
-		System.out.println(LOGGER.getName());
-		System.out.println(LOGGER.isDebugEnabled());
-		System.out.println(LOGGER.isInfoEnabled());
+        System.out.println(LOGGER.getName());
+        System.out.println(LOGGER.isDebugEnabled());
+        System.out.println(LOGGER.isInfoEnabled());
 
-		LOGGER.info("【么么哒】【开始执行】【亲爱的{}】", "李双");
-		// slf4j 支持占位符，相比log4j 节省内存，性能高效。
-		LOGGER.info("【参数：】【{}，{}，{}，{}，{}，{}，{}，{}】", "李双", "sdfsadf", "sdf", "fef", "李双", "sdfsadf", "sdf", "fef");
-	}
+        LOGGER.info("【么么哒】【开始执行】【亲爱的{}】", "李双");
+        // slf4j 支持占位符，相比log4j 节省内存，性能高效。
+        LOGGER.info("【参数：】【{}，{}，{}，{}，{}，{}，{}，{}】", "李双", "sdfsadf", "sdf", "fef", "李双", "sdfsadf", "sdf", "fef");
+    }
 
 }
