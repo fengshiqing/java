f1fe745d5dc180dd36a2303f56a5f932b91f3e57
fengshiqing <938481169@qq.com>
2019-06-07 10:51:48 +0800

    代码优化

--- Spring-Boot/src/main/java/com/kunning/controller/SampleController.java
+++ Spring-Boot/src/main/java/com/kunning/controller/SampleController.java
@@ -2,14 +2,16 @@ package com.kunning.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@RestController
+@Controller
 @Api(tags = "SampleController", description = "用户接口")
 public class SampleController {
 
@@ -17,6 +19,7 @@ public class SampleController {
 
 	@ApiOperation(value = "接口文档111", notes = "")
 	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
 	public String hello111() {
 		LOGGER.info("【hello】【开始执行】");
 
@@ -25,10 +28,19 @@ public class SampleController {
 
 	@ApiOperation(value = "接口文档222", notes = "")
 	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@ResponseBody
 	public String hello222() {
 		LOGGER.info("【hello】【开始执行】");
 
 		return "Hello World!";
+	}
+
+	@ApiOperation(value = "接口文档222", notes = "")
+	@RequestMapping(value = "/uploadStatus", method = {RequestMethod.GET, RequestMethod.POST})
+	public String hello333() {
+		LOGGER.info("【hello】【开始执行】");
+
+		return "uploadStatus";
 	}
 
 }
