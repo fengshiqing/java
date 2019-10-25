ff820b9ba0a3fa7912c88822615554f5a65d618a
fengshiqing <938481169@qq.com>
2019-10-26 00:41:22 +0800

    代码优化，优化包名

--- JavaSE/src/main/java/com/kunning/javaSE/algorithm/RandomNum.java
+++ JavaSE/src/main/java/com/kunning/javaSE/algorithm/RandomNum.java
@@ -11,7 +11,7 @@ public class RandomNum {
 	public static void main(String[] args) {
 		// 产生一个[0，1)之间的随机数,大于等于 0.0 且小于 1.0 的[0,1)范围内的 double 类型的数
 		System.out.println("[0,1)范围内的随机数：" + Math.random());//Random类也可以生成随机数，具体的看API文档
-		System.out.println("[1,10)范围内的随机数：" + 1 + Math.random()*9);//从1到10的int型随数
+		System.out.println("[1,10)范围内的随机数：" + (1 + Math.random()*9));//从1到10的int型随数
 		System.out.println("获取范围内随机数：" + suijishu(1,10));
 		//suijiString();
 		//System.out.println(randNum(1,10));
