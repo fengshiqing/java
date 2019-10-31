e617e3059323e5c0e26c5b97019cfc154b9c89a6
fengshiqing <938481169@qq.com>
2019-10-31 20:46:39 +0800

    代码优化

--- SpringBoot/src/main/java/com/kunning/springboot/utils/CipherUtil.java
+++ SpringBoot/src/main/java/com/kunning/springboot/utils/CipherUtil.java
@@ -6,32 +6,42 @@ import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
 
 /**
- * 功能描述：MD5 摘要工具类。
+ * 功能简述：密码工具类。
+ * MD5摘要、Base64编码。
  */
-public class MD5Util {
+public class CipherUtil {
 
     /**
      * 日志
      */
-    private static final Logger LOGGER = LoggerFactory.getLogger(MD5Util.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CipherUtil.class);
 
     /**
      * 功能描述：获取 MD5 摘要。
      *
      * @param plaintext 明文
-     * @param salt 盐值
+     * @param salt      盐值
      *
      * @return MD5摘要
      */
-    public static String getMD5(String plaintext, String salt) {
+    public static String getMd5Digest(String plaintext, String salt) {
         if (StringUtils.isEmpty(plaintext)) {
+            LOGGER.error("【plaintext不能为空！】");
             return null;
         }
         byte[] byteArr = StringUtils.isEmpty(salt) ? plaintext.getBytes() : (plaintext + salt).getBytes();
         return DigestUtils.md5DigestAsHex(byteArr);
     }
 
-    public static void main(String[] args) {
-        getMD5("123", null);
+
+    /**
+     * 功能描述：base64编码
+     *
+     * @param plaintext 明文
+     */
+    public void base64(String plaintext) {
+        // 用这个
+        // spring-core-5.2.0.RELEASE.jar!\org\springframework\util\Base64Utils.class
     }
+
 }
--- SpringBoot/src/test/java/com/kunning/SpringBoot/utils/CipherUtilTest.java
+++ SpringBoot/src/test/java/com/kunning/SpringBoot/utils/CipherUtilTest.java
@@ -4,11 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class MD5UtilTest {
+public class CipherUtilTest {
 
     @Test
     public void getMD5() {
-        String digest = MD5Util.getMD5("", "");
+        String digest = CipherUtil.getMd5Digest("", "");
         System.out.println("【摘要：】" + digest);
+    }
+
+    @Test
+    public void base64() {
     }
 }
