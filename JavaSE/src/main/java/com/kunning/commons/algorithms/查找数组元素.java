f984d05fdcab626e240c3ee5e16eddeb0db7e164
fengshiqing <938481169@qq.com>
2019-11-24 22:26:59 +0800

    代码优化

--- JavaSE/src/main/java/com/kunning/commons/algorithms/查找数组元素.java
+++ JavaSE/src/main/java/com/kunning/commons/algorithms/查找数组元素.java
@@ -1,12 +1,17 @@
 package com.kunning.commons.algorithms;
 
+import java.util.Arrays;
+
 public class 查找数组元素 {
 
 	public static void main(String[] args) {
 		int[] number = {1,3,5,7,9,11,13};
 		//int index = getIndex(number, 5);
 		//System.out.println("index=" + index);
-		
+		int [] numArr = new int[10];
+		System.arraycopy(number, 0, numArr, 0, 7);
+		System.out.println(Arrays.toString(numArr));
+
 		System.out.println(halfSearch(number,10));
 	}
 	
