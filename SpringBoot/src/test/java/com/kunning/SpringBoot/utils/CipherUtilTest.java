878b704601b00c6c1867d9fd61c44454d6c5cbc9
fengshiqing <938481169@qq.com>
2019-12-08 22:53:14 +0800

    代码优化

--- SpringBoot/src/test/java/com/kunning/SpringBoot/utils/CipherUtilTest.java
+++ SpringBoot/src/test/java/com/kunning/SpringBoot/utils/CipherUtilTest.java
@@ -1,10 +1,6 @@
 package com.kunning.springboot.utils;
 
 import org.junit.Test;
-import org.junit.jupiter.api.RepeatedTest;
-import org.springframework.util.Base64Utils;
-
-import static org.junit.Assert.*;
 
 public class CipherUtilTest {
 
@@ -20,8 +16,8 @@ public class CipherUtilTest {
 
     @Test
     public void Encrypt() throws Exception {
-        System.out.println("【加密：】" + CipherUtil.Encrypt("123", "1234123412341234", "0102030405060708"));
-        System.out.println("【解密：】" + CipherUtil.Decrypt("yxjD73tZf9daDDzsACDxEg==", "1234123412341234", "0102030405060708"));
+        System.out.println("【加密：】" + CipherUtil.Encrypt("123", "1234123412341234"));
+        System.out.println("【解密：】" + CipherUtil.Decrypt("yxjD73tZf9daDDzsACDxEg==", "1234123412341234"));
     }
 
 }
