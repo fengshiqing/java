dff7417192b882c01f13f9b5d6b0c39a1e12f742
fengshiqing <938481169@qq.com>
2019-12-08 17:04:14 +0800

    springboot发送邮件

---
+++ SpringBoot/src/main/java/com/kunning/springboot/service/SendMailService.java
@@ -0,0 +1,54 @@
+package com.kunning.springboot.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ * 功能描述：邮件服务类
+ *
+ * @author 冯仕清
+ * @since 2019年10月01日
+ */
+@Service
+public class SendMailService {
+
+    /**
+     * 日志
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SendMailService.class);
+
+    /**
+     * Spring 自带的邮件Sender，会自动读取 application.properties 中的配置
+     */
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    /**
+     * 功能描述：发送电子邮件。
+     *
+     * @param from    发送人
+     * @param to      接收人
+     * @param subject 邮件主题
+     * @param text    邮件内容
+     */
+    public void sendSimpleMail(String from, String to, String subject, String text) {
+        LOGGER.info("【sendSimpleMail】【start】【from:{}, to:{}, subject:{}】", from, to, subject);
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(from); // 发送人
+        simpleMailMessage.setTo(to); // 接收人
+        simpleMailMessage.setSubject(subject); // 邮件主题
+        simpleMailMessage.setText(text); // 邮件内容
+        try {
+            javaMailSender.send(simpleMailMessage);
+        } catch (MailException e) {
+            LOGGER.error("【sendSimpleMail】【邮件发送失败】", e);
+        }
+        LOGGER.info("【sendSimpleMail】【end】【发送邮件成功】");
+    }
+
+}
---
+++ SpringBoot/src/test/java/com/kunning/SpringBoot/service/SendMailServiceTest.java
@@ -0,0 +1,23 @@
+package com.kunning.springboot.service;
+
+import com.kunning.springboot.Application;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+class SendMailServiceTest {
+
+    @Autowired
+    SendMailService sendMailService;
+
+    @Test
+    void sendSimpleMail() {
+        sendMailService.sendSimpleMail("938481169@qq.com", "938481169@qq.com", "【邮件主题】", "【邮件内容】");
+    }
+}
