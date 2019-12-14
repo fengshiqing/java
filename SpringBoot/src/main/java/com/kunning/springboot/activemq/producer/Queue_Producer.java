065f06f1b4ccab9b1383cd132e02bd6173e33ad1
fengshiqing <938481169@qq.com>
2019-12-14 17:31:00 +0800

    代码格式优化

--- SpringBoot/src/main/java/com/kunning/springboot/activemq/producer/Queue_Producer.java
+++ SpringBoot/src/main/java/com/kunning/springboot/activemq/producer/Queue_Producer.java
@@ -1,6 +1,5 @@
 package com.kunning.springboot.activemq.producer;
 
-import com.kunning.springboot.config.ExecutorConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
