a7e68768811e8402f103864780d98b1cafe061d2
fengshiqing <938481169@qq.com>
2019-12-15 23:35:17 +0800

    代码优化

--- JavaWeb/src/main/java/com/kunning/web/servlet/filter/Filter_01.java
+++ JavaWeb/src/main/java/com/kunning/web/servlet/filter/Filter_01.java
@@ -1,5 +1,9 @@
 package com.kunning.web.servlet.filter;
 
+import com.kunning.web.servlet.listener.MyHttpSessionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 import javax.servlet.Filter;
@@ -10,24 +14,29 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 /**
- * <过滤器>
- * 
- * @author kunning
+ * 功能描述：自定义 Filter 过滤器
  *
+ * @author 冯仕清
+ * @since 2019/12/15
  */
 public class Filter_01 implements Filter {
 
+	/**
+	 * 日志
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(Filter_01.class);
+
 	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		System.out.println("【Filter_01初始化了】");
+	public void init(FilterConfig filterConfig) {
+		LOGGER.info("【Filter_01初始化了】");
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("【Filter_01拦截到了请求】");
+		LOGGER.info("【Filter_01拦截到了请求】");
 		chain.doFilter(request, response);
-		System.out.println("【Filter_01放行了请求】");
+		LOGGER.info("【Filter_01已经放行了请求】");
 	}
 
 	@Override
