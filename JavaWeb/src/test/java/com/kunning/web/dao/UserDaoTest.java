5fd3b0df2cdd976a7d36b8d13e4e5ddafc44f7e0
fengshiqing <938481169@qq.com>
2019-12-19 21:36:50 +0800

    代码优化

--- JavaWeb/src/test/java/com/kunning/web/dao/UserDaoTest.java
+++ JavaWeb/src/test/java/com/kunning/web/dao/UserDaoTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
 @WebAppConfiguration
-@ContextConfiguration(locations = {"classpath:spring-mvc.xml"})
+@ContextConfiguration(locations = {"classpath:spring/spring-mvc.xml"})
 public class UserDaoTest {
 
     /**
--- JavaWeb/src/test/java/com/kunning/web/test/Test.java
+++ JavaWeb/src/test/java/com/kunning/web/test/Test.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
-@ContextConfiguration(locations = { "classpath:spring-mvc.xml" })
+@ContextConfiguration(locations = {"classpath:spring/spring-mvc.xml"})
 public class Test {
 
 
