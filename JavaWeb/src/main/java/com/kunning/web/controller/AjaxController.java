5801b5a48521729e3f704e895911cea622b78ade
fengshiqing <938481169@qq.com>
2020-03-11 23:40:49 +0800

    ajax示例

---
+++ JavaWeb/src/main/java/com/kunning/web/controller/AjaxController.java
@@ -0,0 +1,33 @@
+package com.kunning.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class AjaxController {
+
+    /**
+     * 日志
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(AjaxController.class);
+
+    /**
+     * 功能描述：处理Ajax请求
+     *
+     * @param map 前端传过来的参数
+     */
+    @PostMapping(value="/SpringMVC/dealAjaxReq")
+    public void dealAjaxReq(@RequestBody Map<String, String> map) {
+        LOGGER.info("【dealAjaxReq】【开始执行】【请求参数：】【map:{}】", map);
+        for(Map.Entry<String, String> entry: map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+        LOGGER.info("【dealAjaxReq】【结束执行】");
+    }
+
+}
