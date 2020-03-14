c1e9106c0cf1320df26fdaf35b52e7fbc94602a2
fengshiqing <938481169@qq.com>
2020-03-14 10:05:12 +0800

    代码优化

--- JavaWeb/src/main/webapp/app/static/ajax.js
+++ JavaWeb/src/main/webapp/app/static/ajax.js
@@ -37,6 +37,8 @@ $(document).ready(function () { // 可以简写：$(function(){
 
     // ajax 调用后台接口
     $("#invokeAjax").click(function () {
+        // JSON.parse();//用于将一个 JSON 字符串转换为 JavaScript 对象。
+        // JSON.stringify();// 用于将 JavaScript 值转换为 JSON 字符串。
         const list = {
             name: "fengshiqing",
             email: "938481169@qq.com"
@@ -44,8 +46,8 @@ $(document).ready(function () { // 可以简写：$(function(){
 
         $.ajax({
             type: "POST", // 请求方式:post、get
-            contentType: "application/json;charset=UTF-8", // 请求的媒体类型
             url: "/JavaWeb/SpringMVC/dealAjaxReq", // 请求地址
+            contentType: "application/json;charset=UTF-8", // 请求的媒体类型，也可以这么写"application/json"。这句不加出现415错误:Unsupported Media Type
             data: JSON.stringify(list), // json对象，必须转成json字符串，后台才能正常解析
             //请求成功
             success: function (result) {
