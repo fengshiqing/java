067a33c2cc88d97ea6d03cab3f98b81b0540cccc
fengshiqing <938481169@qq.com>
2020-03-23 22:37:16 +0800

    代码优化

--- SpringBoot/src/main/java/com/kunning/springboot/Application.java
+++ SpringBoot/src/main/java/com/kunning/springboot/Application.java
@@ -1,6 +1,5 @@
 package com.kunning.springboot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication // 标注主程序类，表明这是一个Spring Boot应用
 @EnableScheduling
-@MapperScan("com.kunning.springboot.dao")
 public class Application {
 
     public static void main(String[] args) {
