27efe2d8638a821d2f7d8690e6582ac8946b6c28
fengshiqing <938481169@qq.com>
2020-03-23 23:06:51 +0800

    代码优化

--- SpringBoot/src/main/java/com/kunning/springboot/config/MyBatisConfig.java
+++ SpringBoot/src/main/java/com/kunning/springboot/config/MyBatisConfig.java
@@ -3,7 +3,9 @@ package com.kunning.springboot.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @MapperScan("com.kunning.springboot.dao")
 public class MyBatisConfig {
 
