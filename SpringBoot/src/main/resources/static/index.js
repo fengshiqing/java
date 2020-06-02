03aa2d933ef371ff59a409c4cc76e793b4c1a2c0
fengshiqing <938481169@qq.com>
2020-06-03 00:06:24 +0800

    引入 vue

--- SpringBoot/src/main/resources/static/index.js
+++ SpringBoot/src/main/resources/static/index.js
@@ -2,72 +2,47 @@
  * JS源码文件
  */
 function aaa() {
-	alert("hello");
+    alert("hello");
 }
 
 function ajax() {
-	var xmlhttp;
-	if (window.XMLHttpRequest) {
-	// 所有现代浏览器（IE7+、Firefox、Chrome、Safari 以及 Opera）均内建 XMLHttpRequest 对象（IE5、IE6使用 ActiveXObject）。
-	// XMLHttpRequest 用于在后台与服务器交换数据。这意味着可以在不重新加载整个网页的情况下，对网页的某部分进行更新。
-		xmlhttp = new XMLHttpRequest();// IE7+、Firefox、Chrome、Opera、Safari 浏览器执行代码
-	} else {
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");// IE6, IE5 浏览器执行代码
-	}
-	
-	// 如需将请求发送到服务器，我们使用 XMLHttpRequest 对象的 open() 和 send() 方法：
-	xmlhttp.open("GET", "/redis/setValue", true);
-	xmlhttp.send();
-	
-	xmlhttp.onreadystatechange = function() {// 这个方法可以放在send()方法之后。
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // 所有现代浏览器（IE7+、Firefox、Chrome、Safari 以及 Opera）均内建 XMLHttpRequest 对象（IE5、IE6使用 ActiveXObject）。
+        // XMLHttpRequest 用于在后台与服务器交换数据。这意味着可以在不重新加载整个网页的情况下，对网页的某部分进行更新。
+        xmlhttp = new XMLHttpRequest();// IE7+、Firefox、Chrome、Opera、Safari 浏览器执行代码
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");// IE6, IE5 浏览器执行代码
+    }
+
+    // 如需将请求发送到服务器，我们使用 XMLHttpRequest 对象的 open() 和 send() 方法：
+    xmlhttp.open("GET", "/redis/setValue", true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function () {// 这个方法可以放在send()方法之后。
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 //			document.getElementById("123").innerHTML = xmlhttp.responseText;
-			document.getElementById("123").value = xmlhttp.responseText;
-		}
-	}
-	
+            document.getElementById("123").value = xmlhttp.responseText;
+        }
+    }
+
 }
 
-define("signinApp",["angular"],function(angular){
-	return angular.module("signinApp",[]);
+var vm = new Vue({
+    el: '#app',
+    data: {
+        email:"",
+        telephone:"",
+        password:"",
+        message: 'Hello Vue.js!',
+        newTodoText: '',
+        visitCount: 0,
+        hideCompletedTodos: false,
+        todos: [],
+        error: null
+    }
 });
-
-var signinApp = angular.module('signinApp', []); // ng-app指令指明的应
-signinApp.controller("signinAppController", function($scope, $rootScope, $location, $http){ // 参数必须小写，字母s不能大写
-	$scope.name = "kunning";
-
-	// $scope.username = "";
-	// $scope.email = "";
-	// $scope.password = "";
-
-	console.log("当前页面的URL：" + $location.absUrl());
-
-	$scope.submit = function ($scope) {
-		debugger;
-		var param = {"telephone": $scope.telephone, "email": $scope.email, "password": $scope.password};
-		$http({
-			// method: 'GET',
-			// url: '/signin'
-			method: 'POST',
-			url: '/signin',
-			data: param,
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'}  //重点，不要忘记漏写
-		}).then(function successCallback(response) {
-			debugger;
-			// 请求成功执行代码
-			layer.msg("请求成功");
-			if (response.data.rtnCode === 200) {
-				window.location.href = "./bootstrap-4.5.0-examples/blog/index.html";
-			} else {
-				alert(response.data.rtnMsg);
-			}
-		}, function errorCallback(response) {
-			// 请求失败执行代码
-			layer.msg("请求失败");
-		});
-	}
-
-});
-
-
-
+//
+// console.log(vm.data.email)
+// console.log(vm.data.telephone)
+// console.log(vm.data.password)
