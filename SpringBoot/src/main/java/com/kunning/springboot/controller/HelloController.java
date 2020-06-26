745c1174630b90b36a31a3a8f9296fa7452942a0
fengshiqing <938481169@qq.com>
2020-06-26 15:44:49 +0800

    代码优化

--- SpringBoot/src/main/java/com/kunning/springboot/controller/HelloController.java
+++ SpringBoot/src/main/java/com/kunning/springboot/controller/HelloController.java
@@ -1,34 +1,34 @@
 package com.kunning.springboot.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello 你好";
+    @RequestMapping("/hello")
+    public String hello(HttpServletRequest request){
+        return "hello 你好 " + request.getRemoteUser();
     }
 
-    @GetMapping("/helloAdmin")
+    @RequestMapping("/helloAdmin")
     @PreAuthorize("hasAnyRole('admin')") // 指定访问级别的角色
     public String helloAdmin(){
         return "helloAdmin";
     }
 
-    @GetMapping("/helloUser")
+    @RequestMapping("/helloUser")
     @PreAuthorize("hasAnyRole('admin','normal')")
     public String helloUser(){
         return "helloUser";
     }
 
-    @GetMapping("/session")
+    @RequestMapping("/session")
     public String session(HttpSession session){
         String username = (String) session.getAttribute("_user");
         return "helloUser," + username;
