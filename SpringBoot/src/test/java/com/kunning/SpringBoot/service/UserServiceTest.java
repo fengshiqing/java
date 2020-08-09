bfb88fc61570174a8b982ceee9fb4ac77ca0ad7b
fengshiqing <938481169@qq.com>
2020-08-09 22:42:51 +0800

    代码优化

--- SpringBoot/src/test/java/com/kunning/SpringBoot/service/UserServiceTest.java
+++ SpringBoot/src/test/java/com/kunning/SpringBoot/service/UserServiceTest.java
@@ -13,8 +13,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 class UserServiceTest {
