92b70dc1629b7db13960af5b3dcbdb94e296c5c1
fengshiqing <938481169@qq.com>
2020-08-23 16:56:38 +0800

    代码优化

--- SpringBoot/src/main/java/com/kunning/springboot/service/ActivityService.java
+++ SpringBoot/src/main/java/com/kunning/springboot/service/ActivityService.java
@@ -9,6 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+/**
+ * 功能描述：活动 Service
+ *
+ * @author fengshiqing
+ * @since 2020-08-23
+ */
 @Service
 public class ActivityService {
 
@@ -17,6 +23,14 @@ public class ActivityService {
      */
     private static final Logger LOGGER = LoggerFactory.getLogger(ActivityService.class);
 
+    /**
+     * 功能描述：根据活动ID查询活动名称。
+     * 先查缓存，再查数据库，数据库中查到再回写到缓存。
+     *
+     * @param id 活动ID
+     *
+     * @return 活动名称
+     */
     @RedisCache(key="#id")
     public String queryActivityName(String id) {
         LOGGER.info("【queryActivityName】【start】【id:{}】", id);
