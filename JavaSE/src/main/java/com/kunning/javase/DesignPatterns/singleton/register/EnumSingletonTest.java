7832d1027b4ab3e62450ce92b54037cc2fbba8a7
fengshiqing <938481169@qq.com>
2021-05-05 00:41:41 +0800

    枚举式单例

--- JavaSE/src/main/java/com/kunning/javase/DesignPatterns/singleton/register/EnumSingletonTest.java
+++ JavaSE/src/main/java/com/kunning/javase/DesignPatterns/singleton/register/EnumSingletonTest.java
@@ -4,8 +4,13 @@
 
 package com.kunning.javase.DesignPatterns.singleton.register;
 
+import com.kunning.javase.DesignPatterns.singleton.seriable.SeriableSingleton;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
 
 /**
@@ -16,6 +21,10 @@ import java.lang.reflect.Constructor;
  */
 public class EnumSingletonTest {
 
+    /**
+     * 功能描述：测试枚举式单例能否被“反射”破坏.
+     * 测试结果：枚举天然的能防止 “反射” 导致的多个实例。
+     */
     @Test
     public void test() {
         EnumSingleton instance = EnumSingleton.getInstance();
@@ -27,6 +36,39 @@ public class EnumSingletonTest {
             c.setAccessible(true);
             Object o = c.newInstance(); // 调用这个方法时，JDK会判断是否为枚举，枚举就会报错。
             System.out.println(o);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 功能描述：测试枚举式单例能否被“序列化/饭序列化”破坏.
+     * 测试结果：枚举天然的能防止 “序列化/反序列化” 导致的多个实例。
+     */
+    @Test
+    public void testEnumSingleton() {
+
+        EnumSingleton s1;
+        EnumSingleton s2 = EnumSingleton.getInstance();
+
+        FileOutputStream fos;
+        try {
+
+            fos = new FileOutputStream("EnumSingleton.obj");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(s2);
+            oos.flush();
+            oos.close();
+
+            FileInputStream fis = new FileInputStream("EnumSingleton.obj");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            s1 = (EnumSingleton) ois.readObject();
+            ois.close();
+
+            System.out.println(s1);
+            System.out.println(s2);
+            System.out.println(s1 == s2);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
