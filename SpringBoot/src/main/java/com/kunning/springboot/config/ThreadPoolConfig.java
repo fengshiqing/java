07c8a1fec0bed83d0a862738c7bce123f3e27917
fengshiqing <938481169@qq.com>
2021-05-29 21:22:26 +0800

    Spring线程池

--- SpringBoot/src/main/java/com/kunning/springboot/config/ThreadPoolConfig.java
+++ SpringBoot/src/main/java/com/kunning/springboot/config/ThreadPoolConfig.java
@@ -4,56 +4,47 @@
 
 package com.kunning.springboot.config;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
-
+/**
+ * 功能描述：配置 Spring线程池。
+ *
+ * @author fengshiqing
+ * @since 2019-02-08
+ */
 @Configuration
 public class ThreadPoolConfig {
-
-    /**
-     * 日志
-     */
     private static final Logger LOGGER = LoggerFactory.getLogger(ThreadPoolConfig.class);
 
     /**
      * 构造函数
      */
     public ThreadPoolConfig() {
-        LOGGER.info("【初始化 线程池 配置】");
+        LOGGER.info("【初始化 Spring线程池 配置】");
     }
 
     @Bean
     public Executor springThreadPool() {
-        LOGGER.info("【springThreadPool】【start】");
+        LOGGER.info("【springThreadPool】【config】【start】");
 
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-
-        executor.setCorePoolSize(5); //配置核心线程数
-
-        //配置最大线程数
-        executor.setMaxPoolSize(10);
-
-        //配置队列大小
-        executor.setQueueCapacity(100);
-
-        //配置线程池中的线程的名称前缀
-        executor.setThreadNamePrefix("spring-thread-pool-");
-
-        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
-        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
-
+        executor.setCorePoolSize(10); // 核心线程数，线程池维护的最少线程数量
+        executor.setMaxPoolSize(100); // 配置最大线程数
+        executor.setQueueCapacity(100); // 配置缓冲队列大小，最大线程都不够用时，先存在这个队列里等待
+        executor.setThreadNamePrefix("SpringThreadPool_"); // 配置线程池中的线程的名称前缀
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务。CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
 
-        //执行初始化
-        executor.initialize();
+        executor.initialize();// 执行初始化
 
-        LOGGER.info("【springThreadPool】【end】");
+        LOGGER.info("【springThreadPool】【config】【end】");
         return executor;
     }
 }
