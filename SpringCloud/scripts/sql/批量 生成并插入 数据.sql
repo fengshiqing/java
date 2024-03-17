1f8a43791b6f35112f34243864fcd731ad1b2662
fengshiqing <938481169@qq.com>
2024-03-17 21:49:44 +0800

    优化注释

--- SpringCloud/scripts/sql/批量 生成并插入 数据.sql
+++ SpringCloud/scripts/sql/批量 生成并插入 数据.sql
@@ -1,5 +1,30 @@
 # 批量插入数据
-DROP PROCEDURE IF EXISTS `fengshiqing`.batchInsert;
+DROP PROCEDURE IF EXISTS `vonsiking`.batchInsert;
+DELIMITER $$
+CREATE PROCEDURE `fengshiqing`.batchInsert(IN max_num INT)
+BEGIN
+    DECLARE i INT DEFAULT 0;
+    START TRANSACTION; -- 避免每次都生成一个新的事务，浪费性能
+    WHILE(i < max_num)
+        DO
+            INSERT INTO `vonsiking`.`t_user` (
+                `name`, `code`, `description`, `price`
+            ) VALUES (
+                CONCAT('冯仕清', ROUND(RAND()*1000, 0)),
+                CONCAT('fengshiqing', ROUND(RAND()*1000, 0)),
+                CONCAT('描述信息', ROUND(RAND()*1000, 0)),
+                ROUND(RAND()*1000, 0)
+            );
+            SET i = i + 1;
+    END WHILE;
+    COMMIT;
+END $$
+DELIMITER $$
+
+
+
+# 批量插入数据
+DROP PROCEDURE IF EXISTS `vonsiking`.batchInsert;
 DELIMITER $$
 CREATE PROCEDURE `fengshiqing`.batchInsert(IN max_num INT)
 BEGIN
@@ -8,15 +33,20 @@ BEGIN
     WHILE(i < max_num)
         DO
             INSERT INTO `fengshiqing`.`t_product` (
-                `name`, `code`, `price`
+                `name`, `code`, `description`, `price`
             ) VALUES (
-                CONCAT('冯仕清', ROUND(RAND()*1000, 0)), CONCAT('fengshiqing', ROUND(RAND()*1000, 0)), ROUND(RAND()*1000, 0)
+                CONCAT('冯仕清', ROUND(RAND()*1000, 0)),
+                CONCAT('fengshiqing', ROUND(RAND()*1000, 0)),
+                CONCAT('描述信息', ROUND(RAND()*1000, 0)),
+                ROUND(RAND()*1000, 0)
             );
             SET i = i + 1;
     END WHILE;
     COMMIT;
 END $$
 DELIMITER $$
+
+
 
 # t_product 表。4个字段，插入了100w数据，花费了
 # 执行耗时   : 57.409 sec
