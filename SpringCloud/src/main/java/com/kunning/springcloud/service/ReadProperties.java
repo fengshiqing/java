b122cb6a85e0dff3f640f08b399de6ce3ecaaac4
fengshiqing <938481169@qq.com>
2024-03-30 19:59:40 +0800

    代码优化

--- SpringCloud/src/main/java/com/kunning/springcloud/service/ReadProperties.java
+++ SpringCloud/src/main/java/com/kunning/springcloud/service/ReadProperties.java
@@ -4,23 +4,23 @@
 
 package com.kunning.springcloud.service;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
+@Slf4j
 @Service
 public class ReadProperties implements CommandLineRunner {
 
     private final Environment env;
 
-    public ReadProperties(Environment env) {
-        this.env = env;
-    }
-
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         System.err.println("【local.ip=】" + env.getProperty("local.ip"));
-        System.err.println("spring.application.name -----" + env.getProperty("spring.application.name"));
+        System.err.println("【spring.application.name】" + env.getProperty("spring.application.name"));
         System.err.println("【spring.datasource.type】" + env.getProperty("spring.datasource.type"));
     }
 }
