3a717d29e40c3b2aeaf085b6aca24152797cb15e
fengshiqing <938481169@qq.com>
2024-04-05 14:23:59 +0800

    优化注释

--- SpringCloud/src/main/java/com/kunning/springcloud/aspect/AccessLogAspect.java
+++ SpringCloud/src/main/java/com/kunning/springcloud/aspect/AccessLogAspect.java
@@ -8,7 +8,12 @@ import com.fengshiqing.common.bean.Resp;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
