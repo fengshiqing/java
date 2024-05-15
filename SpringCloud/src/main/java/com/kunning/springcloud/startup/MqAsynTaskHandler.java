7867c4ae92bdcb0c6099e99cc3b22bc9a3e33f9d
fengshiqing <938481169@qq.com>
2024-05-15 22:12:13 +0800

    新增 模板方法模式 的代码

---
+++ SpringCloud/src/main/java/com/kunning/springcloud/startup/MqAsynTaskHandler.java
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2024. fengshiqing 冯仕清. All Rights Reserved.
+ */
+
+package com.kunning.springcloud.startup;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 功能描述：这个好现实模板模式的写法吧
+ */
+public class MqAsynTaskHandler extends MqBaseHandler {
+
+    @PostConstruct
+    public void init() {
+        super.process();
+    }
+
+    public void doService() {
+        // 业务逻辑
+
+    }
+
+}
