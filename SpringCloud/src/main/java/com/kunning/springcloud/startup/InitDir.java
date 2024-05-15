c756a77d59f0c8bb7a8623da16d25a4d136c3603
fengshiqing <938481169@qq.com>
2024-05-15 21:15:24 +0800

    优化代码

--- SpringCloud/src/main/java/com/kunning/springcloud/startup/InitDir.java
+++ SpringCloud/src/main/java/com/kunning/springcloud/startup/InitDir.java
@@ -4,8 +4,7 @@
 
 package com.kunning.springcloud.startup;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
@@ -18,20 +17,20 @@ import java.io.File;
  * @author fengshiqing
  * @since 2022-01-08
  */
+@Slf4j
 @Service
 @Order(0)
 public class InitDir implements CommandLineRunner {
-    private static final Logger LOGGER = LoggerFactory.getLogger(InitDir.class);
 
     @Override
     public void run(String... args) {
-        LOGGER.info("【执行顺序 00】");
+        log.info("【执行顺序 00】");
         System.out.println("【执行顺序 00】");
 
         String usrHome = System.getProperty("user.home"); // 执行当前程序的用户的家目录，windows上是"C:\Users\fengshiqing\"
         File dirPath = new File(usrHome + "/temp");
         if (!dirPath.exists() && !dirPath.mkdir()) { // 临时目录不存在且创建失败，就结束掉java进程，手动去创建下。
-            LOGGER.error("【启动项目时，创建必须的文件夹 失败】【请手动去创建下/temp目录】");
+            log.error("【启动项目时，创建必须的文件夹 失败】【请手动去创建下/temp目录】");
             System.exit(-1);
         }
 
