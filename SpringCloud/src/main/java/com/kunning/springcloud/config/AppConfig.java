1ac3e61a1212b72edb4d1e2c94792effb4a5d8c9
fengshiqing <938481169@qq.com>
2024-07-09 23:30:22 +0800

    代码优化

--- SpringCloud/src/main/java/com/kunning/springcloud/config/AppConfig.java
+++ SpringCloud/src/main/java/com/kunning/springcloud/config/AppConfig.java
@@ -6,16 +6,12 @@ package com.kunning.springcloud.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
