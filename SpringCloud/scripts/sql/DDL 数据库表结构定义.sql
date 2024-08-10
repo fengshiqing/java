56ac013e73c3a475ec853a64eaa1cf19f6b6d4b8
fengshiqing <938481169@qq.com>
2024-08-10 23:15:57 +0800

    优化sql、新增表

--- SpringCloud/scripts/sql/DDL 数据库表结构定义.sql
+++ SpringCloud/scripts/sql/DDL 数据库表结构定义.sql
@@ -5,6 +5,7 @@ CREATE DATABASE IF NOT EXISTS vonsiking; -- 默认就是 utf8mb4 编码，utf8mb
 # 建表原则：
 # 使用满足需求的最小长度/大小的字段
 # 能用数字类型，就用数字类型，能用 UNSIGNED 就用 UNSIGNED，参考：https://deepinout.com/mysql/mysql-questions/t_what-is-unsigned-in-mysql.html
+# 主键最好不要用 BIGINT UNSIGNED，最好用BIGINT，因为 BIGINT 对应 java 中的基本类型long，性能高，而无符号的BIGINT只能使用Biginteger类型来处理，性能略低
 # 每个字段都尽量 设置 NOT NULL
 # 每个字段都尽量 设置 DEFAULT 值
 # 更新时间 使用 ON UPDATE NOW() 自动更新
@@ -13,8 +14,8 @@ CREATE DATABASE IF NOT EXISTS vonsiking; -- 默认就是 utf8mb4 编码，utf8mb
 # 新建 t_user 表
 # DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE IF NOT EXISTS `t_user` (
-    `id`                       BIGINT UNSIGNED                 AUTO_INCREMENT    PRIMARY KEY       COMMENT '自增主键ID',
-    `userId`                   BIGINT UNSIGNED     NOT NULL                                        COMMENT '用户ID',
+    `id`                       BIGINT                          AUTO_INCREMENT    PRIMARY KEY       COMMENT '自增主键ID',
+    `userId`                   BIGINT              NOT NULL                                        COMMENT '用户ID',
     `password`                 VARCHAR(128)        NOT NULL                                        COMMENT '用户登录密码',
     `username`                 VARCHAR(32)         NOT NULL                                        COMMENT '用户名',
     `gender`                   TINYINT             NOT NULL    DEFAULT 0                           COMMENT '性别：0保密、1男性、2女性',
@@ -30,17 +31,19 @@ CREATE TABLE IF NOT EXISTS `t_user` (
     UNIQUE INDEX t_user_index_userId (userId)
 ) COMMENT '用户表';
 
+
+
 # 新建 t_product 表
 CREATE TABLE IF NOT EXISTS `t_product` (
-    `id`             BIGINT UNSIGNED                AUTO_INCREMENT    PRIMARY KEY       COMMENT '自增主键ID',
+    `userId`         BIGINT                         AUTO_INCREMENT    PRIMARY KEY       COMMENT '自增主键ID',
     `name`           VARCHAR(32)        NOT NULL                                        COMMENT '产品名称',
     `code`           VARCHAR(32)        NOT NULL                                        COMMENT '产品编码',
     `description`    VARCHAR(128)           NULL    DEFAULT NULL                        COMMENT '产品描述',
     `price`          DECIMAL(16,4)          NULL    DEFAULT NULL                        COMMENT '产品价格',
 
-    `create_user`    BIGINT UNSIGNED        NULL                                        COMMENT '创建人',
+    `create_user`    BIGINT             NOT NULL                                        COMMENT '创建人',
     `create_time`    DATETIME           NOT NULL    DEFAULT NOW()                       COMMENT '创建时间',
-    `update_user`    BIGINT UNSIGNED    NOT NULL                                        COMMENT '修改人',
+    `update_user`    BIGINT             NOT NULL                                        COMMENT '修改人',
     `update_time`    DATETIME           NOT NULL    DEFAULT NOW()    ON UPDATE NOW()    COMMENT '修改时间',
     `del_flag`       TINYINT            NOT NULL    DEFAULT 0                           COMMENT '删除标记：0未删除、1已删除'
 ) COMMENT '产品表';
@@ -49,3 +52,34 @@ CREATE TABLE IF NOT EXISTS `t_product` (
 # 将 products 表的数据复制到 t_product 表
 INSERT INTO t_product  (id, `name`, `code`, price) SELECT  id, `name`, `code`, price FROM products;
 
+
+
+# 新建 t_user_flow 表
+CREATE TABLE IF NOT EXISTS `t_user_flow` (
+    `user_id`          BIGINT             NOT NULL                                        COMMENT '用户ID',
+    `flow_id`          VARCHAR(32)        NOT NULL                                        COMMENT '流程ID',
+    `flow_type`        VARCHAR(32)        NOT NULL                                        COMMENT '流程类型',
+    `current_approver` VARCHAR(32)        NOT NULL                                        COMMENT '当前审批人',
+    `remark`           VARCHAR(1024)      NOT NULL                                        COMMENT '申请原因/备注',
+
+    `create_user`      BIGINT             NOT NULL                                        COMMENT '创建人',
+    `create_time`      DATETIME           NOT NULL    DEFAULT NOW()                       COMMENT '创建时间',
+    `update_user`      BIGINT             NOT NULL                                        COMMENT '修改人',
+    `update_time`      DATETIME           NOT NULL    DEFAULT NOW()    ON UPDATE NOW()    COMMENT '修改时间',
+    `del_flag`         TINYINT            NOT NULL    DEFAULT 0                           COMMENT '删除标记：0未删除、1已删除',
+    PRIMARY KEY (user_id, flow_id)
+) COMMENT '用户流程表，各种流程：请假流程、加班流程、费用报销流程等等';
+
+# 新建 t_user_flow_detail 表
+CREATE TABLE IF NOT EXISTS `t_user_flow_detail` (
+    `user_id`          BIGINT             NOT NULL                                        COMMENT '用户ID',
+    `flow_id`          VARCHAR(32)        NOT NULL                                        COMMENT '流程ID',
+    `detail_info`      VARCHAR(1024)      NOT NULL                                        COMMENT '详细信息',
+
+    `create_user`      BIGINT             NOT NULL                                        COMMENT '创建人',
+    `create_time`      DATETIME           NOT NULL    DEFAULT NOW()                       COMMENT '创建时间',
+    `update_user`      BIGINT             NOT NULL                                        COMMENT '修改人',
+    `update_time`      DATETIME           NOT NULL    DEFAULT NOW()    ON UPDATE NOW()    COMMENT '修改时间',
+    `del_flag`         TINYINT            NOT NULL    DEFAULT 0                           COMMENT '删除标记：0未删除、1已删除',
+    PRIMARY KEY (user_id, flow_id)
+) COMMENT '用户流程的详细信息，请假流程中的请假时间、加班流程中的加班时间、费用报销流程中的费用信息等等';
