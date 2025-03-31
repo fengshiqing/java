58e941d99203d989a9ca64fe86fa0dfa945dd6c2
fengshiqing <938481169@qq.com>
2025-03-31 23:02:06 +0800

    ip地址修改

--- SpringCloud/src/main/java/com/fengshiqing/springcloud/service/client/EsClient.java
+++ SpringCloud/src/main/java/com/fengshiqing/springcloud/service/client/EsClient.java
@@ -47,7 +47,7 @@ public class EsClient {
      */
     @PostConstruct
     private void initClient() {
-        String hostname = "192.168.0.100";
+        String hostname = "192.168.2.100";
 
         // 基本的用户名密码认证
         String username = "elastic";
