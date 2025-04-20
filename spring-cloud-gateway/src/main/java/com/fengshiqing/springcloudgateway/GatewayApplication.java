1db545eb0732d0103c1447598974c89c98c1fbe2
fengshiqing <938481169@qq.com>
2025-04-21 00:22:06 +0800

    学习SpringCloudGateway

--- spring-cloud-gateway/src/main/java/com/fengshiqing/springcloudgateway/GatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/fengshiqing/springcloudgateway/GatewayApplication.java
@@ -6,12 +6,15 @@ package com.fengshiqing.springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.handler.predicate.GatewayPredicate;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
+//        GatewayPredicate
+
         ConfigurableApplicationContext applicationContext = SpringApplication.run(GatewayApplication.class, args);
         String userName = applicationContext.getEnvironment().getProperty("user.name");
         String userAge = applicationContext.getEnvironment().getProperty("user.age");
