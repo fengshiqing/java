6dc8de8d063f744006368acd2fc52b046bc185b4
fengshiqing <938481169@qq.com>
2025-05-03 10:48:01 +0800

    代码优化

--- spring-cloud-gateway/src/main/java/com/fengshiqing/springcloudgateway/filter/GlobalLoggerFilter.java
+++ spring-cloud-gateway/src/main/java/com/fengshiqing/springcloudgateway/filter/GlobalLoggerFilter.java
@@ -11,10 +11,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.Date;
-
 /**
  * 功能描述：全局过滤器：统计耗时。
+ * 全局过滤器：拦截所有请求，无需配置就是针对所有的请求。
+ * 局部过滤器：需要在路由中配置后才生效。
+ *
+ * Reactor Netty 访问日志，请设置：-Dreactor.netty.http.server.accessLogEnabled=true 这个配置必须是java系统属性，而不是spring-boot属性。
+ *
  *
  * @author fengshiqing
  * @since 2025-01-01
@@ -26,15 +29,18 @@ public class GlobalLoggerFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         // 前置过滤
-        long beginTimestamp = new Date().getTime();
-        log.info("【全局过滤器：统计耗时】【当前时刻：{}】", beginTimestamp);
+        long start = System.currentTimeMillis();
+        log.info("【全局过滤器】【当前时刻：{}】", start);
+
+        String path = exchange.getRequest().getPath().value();
+        log.info("【全局过滤器】【path：{}】", path);
 
         // 执行 业务逻辑
         Mono<Void> result = chain.filter(exchange);
 
         // 后置过滤
-        long endTimestamp = new Date().getTime();
-        log.info("【全局过滤器：统计耗时】【当前时刻：{} ; 本次执行时长：{} 毫秒】", endTimestamp, endTimestamp - beginTimestamp);
+        long end = System.currentTimeMillis();
+        log.info("【全局过滤器】【当前时刻：{}】【本次执行时长：{} 毫秒】", end, end - start);
 
         return result;
     }
