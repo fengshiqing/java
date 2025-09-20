c7122f12e6e2eed7625d89796127e67513e4457b
fengshiqing <938481169@qq.com>
2025-09-20 08:43:49 +0800

    代码优化

--- JavaWeb/src/main/java/com/kunning/web/controller/UploadController.java
+++ JavaWeb/src/main/java/com/kunning/web/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.kunning.web.controller;
 
 import com.fengshiqing.common.bean.Resp;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -15,9 +16,9 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 @Controller
 public class UploadController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
 
     /**
      * 图片文件上传
@@ -25,7 +26,7 @@ public class UploadController {
     @ResponseBody
     @RequestMapping(value = "/uploadPhoto", method = RequestMethod.POST)
     public Resp uploadPhoto(MultipartFile file, HttpServletRequest request) throws IllegalStateException, IOException {
-        LOGGER.info("【uploadPhoto】【开始执行】");
+        log.info("【uploadPhoto】【开始执行】");
 
         if (file != null) {
             String originalFilename = file.getOriginalFilename();// 上传的原始文件名称
@@ -36,23 +37,23 @@ public class UploadController {
                 String fileName = originalFilename.substring(0, dotIndex);// 不带后缀的文件名
                 String fileType = originalFilename.substring(dotIndex);// 文件后缀类型，带点号
                 String trueFileName = fileName + "_" + System.currentTimeMillis() + fileType;// 设置文件名称上传到服务器上的名称
-                LOGGER.info("【文件的新名称：{}】", trueFileName);
+                log.info("【文件的新名称：{}】", trueFileName);
                 String realPath = request.getSession().getServletContext().getRealPath("/");// 项目在容器中实际发布运行的根路径
                 String path = realPath +/*System.getProperty("file.separator")+*/trueFileName;// 设置图片文件的存放路径
-                LOGGER.info("【存放图片文件的路径：{}】", path);
+                log.info("【存放图片文件的路径：{}】", path);
 
                 file.transferTo(new File(path));// 将文件转存到指定的路径
-                LOGGER.info("【文件成功上传到指定目录下】");
+                log.info("【文件成功上传到指定目录下】");
             } else {
-                LOGGER.error("【上传文件的名称或者类型不正确，请重新上传】");
+                log.error("【上传文件的名称或者类型不正确，请重新上传】");
                 return null;
             }
         } else {
-            LOGGER.error("【上传文件为空】");
+            log.error("【上传文件为空】");
             return null;
         }
 
-        LOGGER.info("【uploadPhoto】【开始执行】【执行成功】");
+        log.info("【uploadPhoto】【开始执行】【执行成功】");
         return new Resp(200, "success");
     }
 
