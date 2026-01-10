9059a0cfc6c55c5a4e7959ea35a083a13dc578d1
fengshiqing <938481169@qq.com>
2026-01-10 22:35:44 +0800

    常量类

---
+++ Common/src/main/java/com/fengshiqing/common/Constant/SysConstant.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022. fengshiqing 冯仕清. All Rights Reserved.
+ */
+
+package com.fengshiqing.common.Constant;
+
+/**
+ * 功能描述：用户相关的常量。
+ *
+ * @author fengshiqing
+ * @since 2024-09-25
+ */
+public interface SysConstant {
+
+    /**
+     * 日志链路追踪
+     */
+    String TRACE_ID = "TRACE_ID";
+
+    /**
+     * 日志链路追踪
+     */
+    String USER_NAME = "USER_NAME";
+
+    /**
+     * 性别
+     */
+    interface GENDER {
+        /**
+         * 男
+         */
+        int gender_1 = 1;
+        /**
+         * 女
+         */
+        int gender_2 = 2;
+    }
+
+}
