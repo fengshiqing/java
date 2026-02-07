16d5f6d11dae6a3ed778294f328c520c39226831
fengshiqing <938481169@qq.com>
2026-02-08 00:21:00 +0800

    代码优化

--- JavaWeb/src/main/java/com/kunning/web/utils/ByUrlGetHtml.java
+++ JavaWeb/src/main/java/com/kunning/web/utils/ByUrlGetHtml.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +23,11 @@ public class ByUrlGetHtml {
 		List<String> strList = new ArrayList<>();
 		for (int i = 0; i < 1; i++) {
 			String url = "https://m.liepin.com/zhaopin/pn" + i + "/?keyword=%E5%A4%A7%E6%95%B0%E6%8D%AE";
-			String htmlSourceCode = getHtmlSourceCode(url, "utf-8");
+			String htmlSourceCode = getHtmlSourceCode(url);
 			Document document = Jsoup.parse(htmlSourceCode);// 转换为DOM树
 			Elements jobList = document.getElementsByClass("job-card");
 			for (Element job : jobList) {
-				String joburl = job.getElementsByClass("flexbox").get(0).getElementsByTag("a").attr("href");
+				String joburl = job.getElementsByClass("flexbox").getFirst().getElementsByTag("a").attr("href");
 
 				if (joburl.endsWith(".shtml")) {
 					strList.add(joburl);
@@ -36,9 +37,9 @@ public class ByUrlGetHtml {
 
 		StringBuilder sb = new StringBuilder();
 		for (String str : strList) {
-			String jobDesc = getHtmlSourceCode(str, "utf-8");
+			String jobDesc = getHtmlSourceCode(str);
 			Document document = Jsoup.parse(jobDesc);// 转换为DOM树
-			Element jobcontent = document.getElementsByClass("content-word").get(0);
+			Element jobcontent = document.getElementsByClass("content-word").getFirst();
 			String[] split = jobcontent.text().split("[^a-zA-Z]+");
 			for (String word : split) {
 				sb.append(word.toLowerCase()).append("\t");
@@ -57,11 +58,11 @@ public class ByUrlGetHtml {
 	 * 
 	 * @throws Exception
 	 */
-	private static String getHtmlSourceCode(String urlStr, String charset) throws Exception {
+	private static String getHtmlSourceCode(String urlStr) throws Exception {
 		URLConnection urlConn = new URL(urlStr).openConnection();// 建立并打开连接
 		InputStream in = urlConn.getInputStream();// 获取源代码 IO流，就是管道
 		// in.read();// 不用这个流的这个方法，原因有二：1、一个字节一个字节的读取，很慢；2、频繁的读取磁盘，给磁盘造成很大的压力
-		BufferedReader bfr = new BufferedReader(new InputStreamReader(in, charset));// 建立缓冲流
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));// 建立缓冲流
 		// 4.3、从缓冲流中读取数据
 		StringBuilder sb = new StringBuilder();
 		String line;
