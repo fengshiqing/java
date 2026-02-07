0ceb9acc8b90d3cd9445ff8199e5e9c78423d957
fengshiqing <938481169@qq.com>
2026-02-07 23:15:57 +0800

    代码优化

---
+++ common/src/main/java/com/fengshiqing/common/LinkedListTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) fengshiqing 冯仕清 2026. All Rights Reserved.
+ */
+
+package com.fengshiqing.common;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		DuiLie dl = new DuiLie();
+		dl.myadd("java001");
+		dl.myadd("java002");
+		dl.myadd("java003");
+		dl.myadd("java004");
+		System.out.println(dl);
+		
+		while(!dl.isNull()) {
+			System.out.println(dl.myGet());
+		}
+	}
+
+	static class DuiLie {//封装链表类LinkedList，实现先进先出。
+		private final LinkedList<Object> link;
+		DuiLie() {
+			link = new LinkedList<>();
+		}
+
+		void myadd(Object obj) {
+			link.addFirst(obj);
+		}
+
+		Object myGet() {
+			return link.removeLast();//先进先出，如果要实现先进后出，就用 removeFirst()
+		}
+
+		boolean isNull() {
+			return link.isEmpty();
+		}
+	}
+
+}
