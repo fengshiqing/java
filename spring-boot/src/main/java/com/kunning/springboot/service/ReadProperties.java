9092f505b5b2fcaa3b107b1409d7cfa38532c4ac
fengshiqing <938481169@qq.com>
2026-02-21 19:46:46 +0800

    代码优化

--- spring-boot/src/main/java/com/kunning/springboot/service/ReadProperties.java
+++ spring-boot/src/main/java/com/kunning/springboot/service/ReadProperties.java
@@ -9,28 +9,25 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 /**
  * 功能描述：读取配置项。
- *
- * CommandLineRunner 和 ApplicationRunner 的区别看这里：https://blog.csdn.net/m0_37739193/article/details/85255317
+ * <p>
+ * CommandLineRunner 和 ApplicationRunner 的区别看这里：<a href="https://blog.csdn.net/m0_37739193/article/details/85255317">...</a>
  *
  * @author 冯仕清
  * @since 2021-06-24
  */
+@Order(10)
 @Service
 @PropertySource(value = {"classpath:config.properties"})
-public class ReadProperties implements CommandLineRunner, Ordered, ApplicationRunner {
+public class ReadProperties implements CommandLineRunner, ApplicationRunner {
 
     @Value("${name}")
     private String name;
 
-    @Override
-    public int getOrder() {
-        return 10;
-    }
 
     @Override
     public void run(String... args) throws Exception {
