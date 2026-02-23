31d2a1d061082d10336b3e07d290005266c451bd
fengshiqing <938481169@qq.com>
2026-02-23 13:23:12 +0800

    删除无用代码

--- spring-cloud/src/main/java/com/fengshiqing/springcloud/config/MyBatisConfig.java
+++ spring-cloud/src/main/java/com/fengshiqing/springcloud/config/MyBatisConfig.java
@@ -6,7 +6,6 @@ package com.fengshiqing.springcloud.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 /**
