9dc6e29473efa74649c593f3081978935092dc74
fengshiqing <c-vonsiking.feng@cn.mcd.com>
2026-07-06 16:58:07 +0800

    基础框架参数优化

---
+++ spring-boot/src/main/java/com/kunning/springboot/model/req/PageReq.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 南京瑞迪水利信息科技有限公司. All rights reserved.
+ */
+
+package com.kunning.springboot.model.req;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 功能描述：分页查询时，用到的分页参数
+ *
+ * @author fengshiqing
+ * @since 2025-08-27
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+@Schema(description = "分页查询时，用到的分页参数")
+public class PageReq {
+
+    @Schema(description = "当前页码")
+    @NotNull(message = "【当前页码 pageNum 不能为空】")
+    @Min(value = 1, message = "【当前页码 pageNum 不能小于 1】")
+    private Integer pageNum;
+
+    @Schema(description = "每页的数据条数")
+    @NotNull(message = "【每页的数据条数 pageSize 不能为空】")
+    @Min(value = 1, message = "【每页的数据条数 pageSize 不能小于 1】")
+    @Max(value = 100, message = "【每页的数据条数 pageSize 不能大于 100】")
+    private Integer pageSize;
+
+    @Schema(description = "偏移量，sql语句中 limit 用到此参数")
+    public int getOffset() {
+        return (pageNum - 1) * pageSize;
+    }
+
+}
--- spring-boot/src/main/java/com/kunning/springboot/model/resp/Resp.java
+++ spring-boot/src/main/java/com/kunning/springboot/model/resp/Resp.java
@@ -4,11 +4,13 @@
 
 package com.kunning.springboot.model.resp;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.slf4j.MDC;
 
 /**
  * 功能描述：接口响应。
@@ -25,6 +27,10 @@ public class Resp {
 
     private int code;
 
+    // 有的系统用 msg，有的用 message，这里两个 属性都定义下。
+    @JsonAlias({"msg", "message"}) // 这个注解只会对入参其起作用，出参中的属性永远都是 message！
     private String message;
+
+    private String traceId = MDC.get("traceId");
 
 }
