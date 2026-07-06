fe89c960e5d4da8171767d7e87f27c3a8407fe73
fengshiqing <c-vonsiking.feng@cn.mcd.com>
2026-07-06 17:01:49 +0800

    基础框架参数优化

--- spring-boot/src/main/java/com/kunning/springboot/model/req/PageReq.java
+++ spring-boot/src/main/java/com/kunning/springboot/model/req/PageReq.java
@@ -27,6 +27,7 @@ public class PageReq {
     @Schema(description = "当前页码")
     @NotNull(message = "【当前页码 pageNum 不能为空】")
     @Min(value = 1, message = "【当前页码 pageNum 不能小于 1】")
+    @Max(value = 100, message = "【最大页码 pageNum 支持 100，请优化其他筛选条件】") // 避免深分页
     private Integer pageNum;
 
     @Schema(description = "每页的数据条数")
@@ -34,6 +35,7 @@ public class PageReq {
     @Min(value = 1, message = "【每页的数据条数 pageSize 不能小于 1】")
     @Max(value = 100, message = "【每页的数据条数 pageSize 不能大于 100】")
     private Integer pageSize;
+
 
     @Schema(description = "偏移量，sql语句中 limit 用到此参数")
     public int getOffset() {
